d55ebe03356ecfa8b4a55b3a0aa89a4e631d5dce
Alexis-zn <alexis.zentici@gmail.com>
2021-04-01 16:32:26 +0200

    updt

---
+++ classInfo.js
@@ -0,0 +1,43 @@
+// class template i'll use for my objects.
+class Human {
+  constructor(firstName, lastName, age, language){
+    this.firstName = firstName
+    this.lastName = lastName
+    this.age = age
+    this.language = language
+}
+// Function: return informations from an object.  
+  printInfo() {
+    console.log(`firstName: ${this.firstName}`)
+    console.log(`lastName: ${this.lastName}`)
+    console.log(`age: ${this.age}`)
+    console.log(`language: ${this.language}`)
+}
+// Function tthat return a boolean with a minimum number has a condition.  
+  canVote() {
+    if(this.age >= 18) {
+    return true;
+  } else {
+    return false;
+  }
+}
+// Function with 2 objects as parameters, to return the array with the most elements.  
+// Not FINISHED /!\  
+  mostSkilledDev() {
+    if(this.language.length > this.language.length){
+    return this.firstName
+  }else if(this.language.length > this.language.length) {
+    return this.firstName
+  } else {
+    return Draw
+  }
+}
+
+}
+// All the var, instance of 'class Human' .
+const alice = new Human('Alice', 'Liddell', 28, ['Javascrip', 'Python', 'Php'] )
+const bob = new Human('Bob', 'Lemon', 30, ['C++', 'Solidity', 'Vyper', 'Golang'])
+const charlie = new Human('Charlie', 'Charlot', 8, ['HTML', 'CSS'])
+
+alice.printInfo()
+console.log(charlie.canVote())
---
+++ info.js
@@ -0,0 +1,61 @@
+// Object literal with 5 keys.
+const alice = {
+  firstName: 'Alice',
+  lastName: 'Liddell',
+  age: 28,
+  language: ['Javascrip', 'Python', 'Php'],
+}
+
+const bob = {
+  firstName: 'Bob',
+  lastName: 'Lemon',
+  age: 30,
+  language: ['C++', 'Solidity', 'Vyper', 'Golang'],
+}
+
+const charlie = {
+  firstName: 'Charlie',
+  lastName: 'Charlot',
+  age: 8,
+  language: ['HTML', 'CSS']
+}
+
+// Function: return informations from an object.
+const printInfo = (obj) => {
+  console.log(`firstName: ${obj.firstName}`)
+  console.log(`lastName: ${obj.lastName}`)
+  console.log(`age: ${obj.age}`)
+}
+
+/* 
+printInfo(alice)
+printInfo(bob)
+printInfo(charlie)
+*/
+
+// Function tthat return a boolean with a minimum number has a condition.
+const canVote = (obj) => {
+  if(obj.age >= 18) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+/*
+console.log(canVote(alice))
+console.log(canVote(bob))
+console.log(canVote(charlie))
+*/
+
+// Function with 2 objects as parameters, to return the array with the most elements.
+const mostSkilledDev = (obj1, obj2) => {
+  if(obj1.language.length > obj2.language.length){
+    return obj1.firstName
+  }else if(obj2.language.length > obj1.language.length) {
+    return obj2.firstName
+  } else {
+    return Draw
+  }
+}
+console.log(mostSkilledDev(bob, alice))
